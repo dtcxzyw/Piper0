e706e462e22e801eb740f1df672be6c7f2f163fd
Yingwei Zheng <dtcxzyw@qq.com>
2022-03-24 02:39:34 +0800

    feat(sync): sync tiles with tev

--- src/Core/Sync.cpp
+++ src/Core/Sync.cpp
@@ -22,77 +22,88 @@
 
 #include <Piper/Core/Common.hpp>
 #include <Piper/Core/Context.hpp>
+#include <Piper/Core/Report.hpp>
 #include <Piper/Core/Sync.hpp>
+#include <Piper/Render/Random.hpp>
+
 #ifdef PIPER_WINDOWS
 // ReSharper disable once CppUnusedIncludeDirective
 #include <sdkddkver.h>
 #endif
+
 #include <boost/asio.hpp>
 #include <type_traits>
 
 PIPER_NAMESPACE_BEGIN
 
 enum class OperationType : char {
-    openImage = 0,
-    reloadImage = 1,
-    closeImage = 2,
-    updateImage = 3,
-    createImage = 4,
-    updateImageV2 = 5,  // Adds multi-channel support
-    updateImageV3 = 6,  // Adds custom striding/offset support
-    openImageV2 = 7,    // Explicit separation of image name and channel selector
+    OpenImage = 0,
+    ReloadImage = 1,
+    CloseImage = 2,
+    UpdateImage = 3,
+    CreateImage = 4,
+    UpdateImageV2 = 5,  // Adds multi-channel support
+    UpdateImageV3 = 6,  // Adds custom striding/offset support
+    OpenImageV2 = 7,    // Explicit separation of image name and channel selector
 };
 
 static_assert(std::is_trivially_copyable_v<OperationType>);
-
-template <typename T>
-concept ContainerLike = requires(T x) {
-    typename T::value_type;
-    x.data();
-    x.size();
-    x.begin();
-    x.end();
-};
 
 class OStream final {
     BinaryData mData{ 4U, context().scopedAllocator };
 
 public:
     template <typename T>
-    requires(std::is_trivially_copyable_v<T>) OStream& operator>>(const T& val) {
+    OStream& emitRaw(const T& val) requires(std::is_trivially_copyable_v<T>) {
         const auto base = reinterpret_cast<const std::byte*>(&val);
         mData.insert(mData.cend(), base, base + sizeof(T));
         return *this;
     }
 
-    OStream& operator>>(size_t) = delete;
+    OStream& operator<<(size_t) = delete;
 
-    template <ContainerLike T>
-    OStream& operator>>(const T& val) {
-        if constexpr(std::is_trivially_copyable_v<typename T::value_type>) {
-            const auto base = reinterpret_cast<const std::byte*>(val.data());
-            mData.insert(mData.cend(), base, base + val.size() * sizeof(typename T::value_type));
-        } else {
-            for(auto& x : val)
-                operator>>(x);
-        }
+    OStream& operator<<(const std::initializer_list<size_t>& val) {
+        const auto base = reinterpret_cast<const std::byte*>(val.begin());
+        mData.insert(mData.cend(), base, base + val.size() * sizeof(size_t));
         return *this;
     }
 
-    OStream& operator>>(const std::string_view val) {
+    OStream& operator<<(const std::initializer_list<std::string_view>& val) {
+        for(auto& v : val)
+            operator<<(v);
+        return *this;
+    }
+
+    OStream& operator<<(const std::span<float>& val) {
+        const auto base = reinterpret_cast<const std::byte*>(val.data());
+        mData.insert(mData.cend(), base, base + val.size_bytes());
+        return *this;
+    }
+
+    OStream& operator<<(const std::string_view val) {
         const auto base = reinterpret_cast<const std::byte*>(val.data());
         mData.insert(mData.cend(), base, base + val.size() + 1);
         mData.back() = static_cast<std::byte>('\0');
         return *this;
     }
 
-    OStream& operator>>(const bool val) {
+    OStream& operator<<(const uint32_t val) {
+        emitRaw(val);
+        return *this;
+    }
+
+    OStream& operator<<(const OperationType val) {
+        emitRaw(val);
+        return *this;
+    }
+
+    OStream& operator<<(const bool val) {
         mData.push_back(static_cast<std::byte>(val ? 1 : 0));
         return *this;
     }
 
     const BinaryData& get() {
-        *reinterpret_cast<uint32_t*>(mData.data()) = static_cast<uint32_t>(mData.size()) - 4;
+        *reinterpret_cast<uint32_t*>(mData.data()) = static_cast<uint32_t>(mData.size());
         return mData;
     }
 };
@@ -101,16 +112,24 @@ class DisplayProviderImpl final : public DisplayProvider {
     static constexpr auto grabFocus = true;
     boost::asio::io_context mCtx;
     boost::asio::ip::tcp::socket mSocket{ mCtx };
+    bool mConnected = false;
+    uint16_t mUniqueID = static_cast<uint16_t>(seeding(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
 
     template <typename... Args>
     void send(Args&&... args) {
         MemoryArena arena;
         OStream stream;
-        (stream >> ... >> std::forward<Args>(args));
+        (stream << ... << std::forward<Args>(args));
 
-        auto& ref = stream.get();
+        auto& data = stream.get();
 
-        mSocket.send(boost::asio::buffer(ref.data(), ref.size()));
+        boost::asio::async_write(mSocket, boost::asio::buffer(data.data(), data.size()),
+                                 [this](const boost::system::error_code ec, size_t /*length*/) {
+                                     if(ec.failed()) {
+                                         error(std::format("Disconnected with tev: {}.", ec.what()));
+                                         mConnected = false;
+                                     }
+                                 });
     }
 
 public:
@@ -118,30 +137,51 @@ public:
         boost::asio::ip::tcp::resolver resolver{ mCtx };
         const auto pos = serverConfig.find(':');
         const auto endpoint = resolver.resolve(serverConfig.substr(0, pos), serverConfig.substr(pos + 1));
-        boost::asio::connect(mSocket, endpoint);
+        boost::system::error_code ec;
+        boost::asio::connect(mSocket, endpoint, ec);
+        if(ec.failed()) {
+            error(std::format("Failed to connect with tev({}). {}.", serverConfig, ec.what()));
+        } else {
+            info(std::format("Successfully connected with tev({}).", serverConfig));
+            mConnected = true;
+        }
     }
     void create(std::string_view imageName, uint32_t width, uint32_t height,
                 const std::initializer_list<std::string_view>& channels) override {
-        send(OperationType::createImage, grabFocus, imageName, width, height, static_cast<uint32_t>(channels.size()), channels);
+        // close(imageName);
+        send(OperationType::CreateImage, grabFocus, imageName, width, height, static_cast<uint32_t>(channels.size()), channels);
     }
     void update(std::string_view imageName, const std::initializer_list<std::string_view>& channels,
                 const std::initializer_list<uint64_t>& offsets, const std::initializer_list<uint64_t>& strides, uint32_t x, uint32_t y,
-                uint32_t width, uint32_t height, const std::pmr::vector<float>& data) override {
-        send(OperationType::updateImageV3, grabFocus, imageName, static_cast<uint32_t>(channels.size()), channels, x, y, width, height,
-             offsets, strides, data);
+                uint32_t width, uint32_t height, const std::span<float>& data) override {
+        size_t stridedImageDataSize = 0;
+        for(uint32_t c = 0; c < channels.size(); ++c)
+            stridedImageDataSize = std::max(stridedImageDataSize, offsets.begin()[c] + (width * height - 1) * strides.begin()[c] + 1);
+
+        const auto stridedData = data.subspan(0, stridedImageDataSize);
+
+        send(OperationType::UpdateImageV3, grabFocus, imageName, static_cast<uint32_t>(channels.size()), channels, x, y, width, height,
+             offsets, strides, stridedData);
     }
     void close(std::string_view imageName) override {
-        send(OperationType::closeImage, imageName);
+        send(OperationType::CloseImage, imageName);
     }
     void disconnect() override {
-        mSocket.close();
+        boost::system::error_code ec;
+        mSocket.shutdown(boost::asio::socket_base::shutdown_both, ec);
+        mSocket.close(ec);
     }
     ~DisplayProviderImpl() override {
         if(mSocket.is_open())
             disconnect();
     }
+
+    uint16_t uniqueID() const noexcept override {
+        return mUniqueID;
+    }
+
     bool isSupported() override {
-        return mSocket.is_open();
+        return mConnected;
     }
 };
 
